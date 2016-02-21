5efc9fda46f9fa5abb498091f6bbcdd71c82f449
Emil Ström <emil.strm@gmail.com>
2016-02-21 17:38:31 +0100

    Parsing done. Triangulation is recommended, looking for fix

--- objparser.js
+++ objparser.js
@@ -1,6 +1,5 @@
 var OBJ = {};
-OBJ.onLoaded = null;
-OBJ.load = function () {
+OBJ.load = function (source, callback) {
 	//REQUEST FILE
 	var rawFile = new XMLHttpRequest();
 
@@ -8,7 +7,9 @@ OBJ.load = function () {
 		if (rawFile.readyState === 4) {
 			if (rawFile.status === 200 || rawFile.status === 0) {
 				var text = rawFile.responseText;
-				OBJ.parse(text);
+				var result = OBJ.parse(text);
+				
+				callback(result);
 			}
 		}
 	}
@@ -18,30 +19,62 @@ OBJ.load = function () {
 }
 
 OBJ.parse = function (text) {
-	var positions = [];
+	var vertexList = [];
+	var uvList = [];
+	var normalList = [];
+	
+	var vertices = [];
 	var uvs = [];
 	var normals = [];
 	
+	//Function to add vertex data
+	var addFace = function (group) {
+		var indeces = group.split("/");
+		
+		for (var i = 0; i < 3; i++) {
+			var index = (parseInt(indeces[0]) - 1) * 3 + i;
+			vertices.push(vertexList[index]);
+		}
+		for (var i = 0; i < 2; i++) {
+			var index = (parseInt(indeces[1]) - 1) * 2 + i;
+			uvs.push(uvList[index]);
+		}
+		for (var i = 0; i < 3; i++) {
+			var index = (parseInt(indeces[2]) - 1) * 3 + i;
+			normals.push(normalList[index]);
+		}
+	}
+
 	var lines = text.split("\n");
 
-	for (var i = 0; i < lines; i++) {
-		var params = lines.split(" ");
-		
-		if (params[0] === "v") { //POSITION
-			positions.push(parseFloat(params[1]));
-			positions.push(parseFloat(params[2]));
-			positions.push(parseFloat(params[3]));
+	for (var i = 0; i < lines.length; i++) {
+		var params = lines[i].split(" ");
+
+		if (params[0] === "v") { //VERTEX
+			for (var j = 1; j <= 3; j++) { vertexList.push(parseFloat(params[j])); }
 		}
 		
 		if (params[0] === "vt") { //UV
-			uvs.push(parseFloat(params[1]));
-			uvs.push(parseFloat(params[2]));
+			for (var j = 1; j <= 2; j++) { uvList.push(parseFloat(params[j])); }
 		}
 		
-		if (params[0] === "vn") { //NORMALS
-			normals.push(parseFloat(params[1]));
-			normals.push(parseFloat(params[2]));
-			normals.push(parseFloat(params[3]));
+		if (params[0] === "vn") { //NORMAL
+			for (var j = 1; j <= 3; j++) { normalList.push(parseFloat(params[j])); }
 		}
+		
+		//FACE
+		if (params[0] === "f") {
+			for (var j = 1; j < params.length; j++) {
+				//If this is the 4th vertex of a face, add the first and second to make a second triangle				
+				if (j > 3) {
+					addFace(params[1]);
+					addFace(params[3]);
+				}
+				
+				addFace(params[j]);
+			}
+		} 
 	}
+	
+	return { positions: vertices, uvs: uvs, normals: normals };
 }
